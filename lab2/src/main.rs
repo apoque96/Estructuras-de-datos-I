953b740b886c73ee847cf8246974322ec075103d
apoque96 <dereckcabrerang@gmail.com>
2024-03-05 21:50:37 -0600

    pending checking services

--- lab2/src/main.rs
+++ lab2/src/main.rs
@@ -5,16 +5,14 @@ use std::{
     path::Path,
 };
 
-fn appartment(app: &Value, wanna_pet_friendly: bool, budget: f64){
+fn find_appartment(appartment: &Value, wanna_pet_friendly: bool, budget: f64) -> Vec<&Value>{
     let mut ans = Vec::new();
-    let app = app.as_array();
-    if app.is_none(){
-        print!("[]");
-        return;
+    let appartment = appartment.as_array();
+    if appartment.is_none(){
+        return ans;
     }
-    let app = app.unwrap();
-    print!("[");
-    for apartment in app{
+    let appartment = appartment.unwrap();
+    for apartment in appartment{
         let pet_friendly = apartment["isPetFriendly"].as_bool();
         if pet_friendly.is_none(){
             continue;
@@ -27,35 +25,97 @@ fn appartment(app: &Value, wanna_pet_friendly: bool, budget: f64){
         }
         let price = price.unwrap();
 
-        if price < budget{
-            if (wanna_pet_friendly == pet_friendly) || wanna_pet_friendly == false{
-                // let id = apartment["id"].as_str();
-                // if id.is_none(){
-                //     continue;
-                // }
-                // let id = id.unwrap();
-                ans.push(apartment);
+        if price <= budget && wanna_pet_friendly == pet_friendly{
+            ans.push(apartment);
+        }
+    }
+    ans
+
+}
+
+fn find_houses(houses: &Value, color: String, budget: f64) -> Vec<&Value>{
+    let mut ans = Vec::new();
+    let houses = houses.as_array();
+    if houses.is_none(){
+        return ans;
+    }
+    let houses = houses.unwrap();
+    for house in houses{
+        let zone = house["zoneDangerous"].as_str();
+        if zone.is_none(){
+            continue;
+        }
+        let zone = zone.unwrap();
+
+        let price = house["price"].as_f64();
+        if price.is_none(){
+            continue;
+        }
+        let price = price.unwrap();
+
+        if price <= budget{
+            match color.as_str() {
+                "Red" => {
+                    if zone == "Red"{
+                        ans.push(house);         
+                    }
+                },
+                "Orange" => {
+                    if zone == "Red" || zone == "Orange"{
+                        ans.push(house);
+                    }
+                },
+                "Yellow" => {
+                    if zone == "Red" || zone == "Orange" || zone == "Yellow"{
+                        ans.push(house);
+                    }
+                },
+                "Green" => {
+                    if zone == "Red" || zone == "Orange" || zone == "Yellow" || zone == "Green"{
+                        ans.push(house);
+                    }
+                }
+                _ => continue,
+            }
+            // ans.push(house);
+        }
+    }
+    ans
+}
+
+fn find_premise(premises: &Value, commercial_activity: String, budget: f64) -> Vec<&Value>{
+    let mut ans = Vec::new();
+    let premises = premises.as_array();
+    if premises.is_none(){
+        return ans;
+    }
+    let premises = premises.unwrap();
+    for premise in premises{
+        let activities = premise["commercialActivities"].as_array();
+        if activities.is_none(){
+            continue;
+        }
+        let activities = activities.unwrap();
+
+        let price = premise["price"].as_f64();
+        if price.is_none(){
+            continue;
+        }
+        let price = price.unwrap();
+
+        for activity in  activities{
+            let activity = activity.as_str();
+            if activity.is_none(){
+                continue;
+            }
+            let activity = activity.unwrap();
+
+            if price <= budget && activity == commercial_activity{
+                ans.push(premise);
             }
         }
     }
-    //Gracias Chatgpt, xd
-    ans.sort_by(|a, b| {
-        // Access the 'price' key from each map
-        let price_a = a["price"].as_f64().unwrap_or(0.0); // default to 0.0 if not present or not a float
-        let price_b = b["price"].as_f64().unwrap_or(0.0); // default to 0.0 if not present or not a float
-
-        // Compare the prices
-        price_a.partial_cmp(&price_b).unwrap_or(std::cmp::Ordering::Equal)
-    });
-
-    for result in ans{
-        let id = result["id"].as_str();
-        if id.is_none(){
-            continue;
-        }
-        let id = id.unwrap();
-        print!("{}, ", id);
-    }
+    ans
 }
 
 // Lee el archivo linea por linea
@@ -67,7 +127,7 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
-//Convierte la linea en el mapa de apartamentos
+//Convierte la linea en el mapa
 fn get_map(ln: &String) -> Result<Value> {
     let v: Value = serde_json::from_str(ln)?;
 
@@ -97,33 +157,81 @@ fn lab2(path: &str){
             break;
         }
         let building_type = building_type.unwrap();
+        let district = map["input1"].as_array();
+        if district.is_none(){
+            continue;
+        }
+        let district = district.unwrap();
+        
+        
+        let budget = map["input2"]["budget"].as_f64();
+        if budget.is_none(){
+            continue;
+        }
+        let budget = budget.unwrap();
+        let mut ans = Vec::new();
         match building_type{
             "Apartments" => {
-                let district = map["input1"].as_array();
-                if district.is_none(){
-                    continue;
-                }
-                let district = district.unwrap();
-                
                 let wanna_pet_friendly = map["input2"]["wannaPetFriendly"].as_bool();
                 if wanna_pet_friendly.is_none() {
                     continue;
                 }
                 let wanna_pet_friendly = wanna_pet_friendly.unwrap();
-
-                let budget = map["input2"]["budget"].as_f64();
-                if budget.is_none(){
-                    continue;
-                }
-                let budget = budget.unwrap();
                 for x in district{
-                    appartment(&x["builds"]["Apartments"], wanna_pet_friendly, budget);
+                    ans.extend(find_appartment(&x["builds"]["Apartments"], wanna_pet_friendly, budget));
                 }
             },
+            "Premises" => {
+                let commercial_activity = map["input2"]["commercialActivity"].as_str();
+                if commercial_activity.is_none(){
+                    continue;
+                }
+                for x in district{
+                    let commercial_activity = String::from(commercial_activity.unwrap());
+                    ans.extend(find_premise(&x["builds"]["Premises"], commercial_activity, budget));
+                }
+            },
+            "Houses" => {
+                let color = map["input2"]["minDanger"].as_str();
+                if color.is_none(){
+                    continue;
+                }
+                for x in district{
+                    let color = String::from(color.unwrap());
+                    ans.extend(find_houses(&x["builds"]["Houses"], color, budget))
+                }
+            }
             _ => continue,
         }
+
+        //Gracias Chatgpt, xd
+        ans.sort_by(|a, b| {
+            // Access the 'price' key from each map
+            let price_a = a["price"].as_f64().unwrap_or(0.0); // default to 0.0 if not present or not a float
+            let price_b = b["price"].as_f64().unwrap_or(0.0); // default to 0.0 if not present or not a float
+
+            // Compare the prices
+            price_a.partial_cmp(&price_b).unwrap_or(std::cmp::Ordering::Equal)
+        });
+        print!("[");
+        {
+        let mut i = 0;
+        while i < ans.len(){
+            let id = ans[i]["id"].as_str();
+            if id.is_none(){
+                continue;
+            }
+            let id = id.unwrap();
+            print!("\"{}\"", id);
+            if i < ans.len()-1{
+                print!(",");
+            }
+            i += 1;
+        }
+        }
+        println!("]");
     }
 }
 fn main() {
-    lab2("./input/input_lab_2_example(2).jsonl");
+    lab2("./input/input_challenge_lab_2(1).jsonl");
 }
